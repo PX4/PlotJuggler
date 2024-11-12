d7f42ea02a9dc93c8aa937cb077733c5e241f825
FARHANG <farhang.nba@gmail.com>
2024-11-12 13:01:55 -0500

    error_collectors clang corrections

--- plotjuggler_plugins/ParserProtobuf/error_collectors.h
+++ plotjuggler_plugins/ParserProtobuf/error_collectors.h
@@ -3,25 +3,29 @@
 
 #include <google/protobuf/io/tokenizer.h>
 #include <google/protobuf/compiler/importer.h>
-
 #include <QStringList>
 
-class IoErrorCollector : public google::protobuf::io::ErrorCollector {
- public:
+class IoErrorCollector : public google::protobuf::io::ErrorCollector
+{
+public:
   void AddError(int line, google::protobuf::io::ColumnNumber column,
                 const std::string& message);
 
   void AddWarning(int line, google::protobuf::io::ColumnNumber column,
                   const std::string& message);
 
-  const QStringList& errors() { return _errors; }
+  const QStringList& errors()
+  {
+    return _errors;
+  }
 
- private:
+private:
   QStringList _errors;
 };
 
-class FileErrorCollector : public google::protobuf::compiler::MultiFileErrorCollector {
- public:
+class FileErrorCollector : public google::protobuf::compiler::MultiFileErrorCollector
+{
+public:
   void AddError(const std::string& filename, int line, int column,
                 const std::string& message);
 
@@ -31,12 +35,18 @@ class FileErrorCollector : public google::protobuf::compiler::MultiFileErrorColl
   void RecordError(absl::string_view filename, int line, int column,
                    absl::string_view message) override;
 
-  const QStringList& errors() const { return _errors; }
+  const QStringList& errors() const
+  {
+    return _errors;
+  }
 
   // Accessor for the collected warnings
-  const QStringList& warnings() const { return _warnings; }
+  const QStringList& warnings() const
+  {
+    return _warnings;
+  }
 
- private:
+private:
   QStringList _errors;
   QStringList _warnings;
 };
