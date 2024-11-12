532e1bb6f7f7ea1d819095cf3689ce5c1927c042
FARHANG <farhang.nba@gmail.com>
2024-11-12 12:55:12 -0500

    clang format correction in error_collector header file

--- plotjuggler_plugins/ParserProtobuf/error_collectors.h
+++ plotjuggler_plugins/ParserProtobuf/error_collectors.h
@@ -6,46 +6,39 @@
 
 #include <QStringList>
 
-class IoErrorCollector : public google::protobuf::io::ErrorCollector
-{
-public:
+class IoErrorCollector : public google::protobuf::io::ErrorCollector {
+ public:
   void AddError(int line, google::protobuf::io::ColumnNumber column,
-                const std::string& message) ;
+                const std::string& message);
 
   void AddWarning(int line, google::protobuf::io::ColumnNumber column,
                   const std::string& message);
 
-  const QStringList& errors()
-  {
-    return _errors;
-  }
+  const QStringList& errors() { return _errors; }
 
-private:
+ private:
   QStringList _errors;
 };
 
-class FileErrorCollector : public google::protobuf::compiler::MultiFileErrorCollector
-{
-public:
-    void AddError(const std::string& filename, int line, int column,
+class FileErrorCollector : public google::protobuf::compiler::MultiFileErrorCollector {
+ public:
+  void AddError(const std::string& filename, int line, int column,
+                const std::string& message);
+
+  void AddWarning(const std::string& filename, int line, int column,
                   const std::string& message);
 
-    void AddWarning(const std::string& filename, int line, int column,
-                    const std::string& message);
-    void RecordError(absl::string_view filename, int line, int column,
-                     absl::string_view message) override;
+  void RecordError(absl::string_view filename, int line, int column,
+                   absl::string_view message) override;
 
-    const QStringList& errors() const {
-        return _errors;
-    }
+  const QStringList& errors() const { return _errors; }
 
-    // Accessor for the collected warnings
-    const QStringList& warnings() const {
-        return _warnings;
-    }
-private:
-    QStringList _errors;
-    QStringList _warnings;
+  // Accessor for the collected warnings
+  const QStringList& warnings() const { return _warnings; }
+
+ private:
+  QStringList _errors;
+  QStringList _warnings;
 };
 
 #endif  // ERROR_COLLECTORS_H
