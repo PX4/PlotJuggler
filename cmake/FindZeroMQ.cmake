d487a0a42510d44f0546c9e73e5d7bc918c7e6e0
farhangnaderi <farhang.nba@gmail.com>
2024-11-17 21:35:02 -0500

    revert zmq CMake

--- cmake/FindZeroMQ.cmake
+++ cmake/FindZeroMQ.cmake
@@ -11,38 +11,14 @@
 # set(ZeroMQ_FOUND TRUE)
 # set(ZeroMQ_INCLUDE_DIRS ${ZMQ_INCLUDE_DIRS})
 # set(ZeroMQ_LIBRARIES ${ZMQ_LIBRARIES})
-# First, try to use PkgConfig to find ZeroMQ
 
-find_package(PkgConfig QUIET)
-if (PKG_CONFIG_FOUND)
-    pkg_check_modules(ZMQ libzmq)
+find_path ( ZeroMQ_INCLUDE_DIR zmq.h )
+find_library ( ZeroMQ_LIBRARY NAMES zmq )
 
-    if (ZMQ_FOUND)
-        message(STATUS "Found ZeroMQ using pkg-config")
-        set(ZeroMQ_FOUND TRUE)
-        set(ZeroMQ_INCLUDE_DIRS ${ZMQ_INCLUDE_DIRS})
-        set(ZeroMQ_LIBRARIES ${ZMQ_LIBRARIES})
-    else()
-        message(WARNING "PkgConfig could not find ZeroMQ, falling back to manual search")
-    endif()
-endif()
+set ( ZeroMQ_LIBRARIES ${ZeroMQ_LIBRARY} )
+set ( ZeroMQ_INCLUDE_DIRS ${ZeroMQ_INCLUDE_DIR} )
 
-# Fallback: if PkgConfig was not found or ZeroMQ was not found with pkg-config, use manual find
-if (NOT ZeroMQ_FOUND)
-    message(STATUS "Attempting to find ZeroMQ using manual search")
-
-    find_path(ZeroMQ_INCLUDE_DIR zmq.h)
-    find_library(ZeroMQ_LIBRARY NAMES zmq)
-
-    set(ZeroMQ_LIBRARIES ${ZeroMQ_LIBRARY})
-    set(ZeroMQ_INCLUDE_DIRS ${ZeroMQ_INCLUDE_DIR})
-
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(ZeroMQ DEFAULT_MSG ZeroMQ_LIBRARY ZeroMQ_INCLUDE_DIR)
-
-    if (ZeroMQ_FOUND)
-        message(STATUS "Found ZeroMQ using manual search")
-    else()
-        message(WARNING "ZeroMQ not found. Some features may be disabled.")
-    endif()
-endif()
+include ( FindPackageHandleStandardArgs )
+# handle the QUIETLY and REQUIRED arguments and set ZeroMQ_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args ( ZeroMQ DEFAULT_MSG ZeroMQ_LIBRARY ZeroMQ_INCLUDE_DIR )
